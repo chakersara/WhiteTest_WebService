9069451f28e8c30c7b69e7eb3a8ecf8b6baa9e5b
Sarra Chaker <65032438+chakersara@users.noreply.github.com>
2022-05-14 19:14:59 +0100

    Update Adress.java

--- src/main/java/de/tekup/demo/soap/consume/whitetest/Adress.java
+++ src/main/java/de/tekup/demo/soap/consume/whitetest/Adress.java
@@ -1,5 +1,6 @@
 
 //Chaker Sarra GLSI-B & Arfaoui Chaima GLSI-A
+
 package de.tekup.demo.soap.consume.whitetest;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -7,6 +8,26 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * <p>Classe Java pour Exam complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="Exam">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="code" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "adress", propOrder = {
     "street",
